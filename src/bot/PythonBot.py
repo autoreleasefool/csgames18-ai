fd9c5588e07a9811e11253a2cd0f95208c89ace6
Joseph Roque <contact@josephroque.ca>
2018-03-23 19:42:14 -0400

    Overwrite pathfinder.create_graph, avoid spikes

--- src/bot/PythonBot.py
+++ src/bot/PythonBot.py
@@ -1,3 +1,4 @@
+import networkx as nx
 from src.bot.Bot import Bot
 from src.symbols.ObjectSymbols import ObjectSymbols
 
@@ -6,6 +7,7 @@ class PythonBot(Bot):
 
     def __init__(self):
         super().__init__()
+        self.pathfinder.create_graph = self.create_graph
 
     def get_name(self):
         return 'Python'
@@ -19,3 +21,29 @@ class PythonBot(Bot):
             return self.commands.move(direction)
         else:
             return self.commands.idle()
+
+    def create_graph(self, game_map):
+        graph = nx.Graph()
+        size_x = len(game_map[0])
+        size_y = len(game_map)
+
+        for y in range(size_y):
+            for x in range(size_x):
+                graph.add_node((y, x))
+
+        for y in range(size_y - 1):
+            for x in range(size_x - 1):
+                symbol = game_map[y][x]
+                if symbol is ObjectSymbols.SPIKE and not self.pathfinder._is_start_or_goal((y, x)):
+                    continue
+
+                if symbol.can_pass_through() or self.pathfinder._is_start_or_goal((y, x)):
+                    right_symbol = game_map[y][x + 1]
+                    if right_symbol.can_pass_through() or self.pathfinder._is_start_or_goal((y, x+1)):
+                        graph.add_edge((y, x), (y, x+1))
+
+                    bottom_symbol = game_map[y + 1][x]
+                    if bottom_symbol.can_pass_through() or self.pathfinder._is_start_or_goal((y+1, x)):
+                        graph.add_edge((y, x), (y+1, x))
+
+        return graph
